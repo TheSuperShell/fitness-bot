120631ec66a090246cdc673a0e4a855f86a24119
Steven Rudenko <srudenko@ncl.com>
2025-10-03 20:35:45 +0300

    add advanced logger

---
+++ src/logger.py
@@ -0,0 +1,39 @@
+import logging
+import logging.handlers
+from queue import Queue
+import sys
+from pathlib import Path
+
+
+def setup_logging() -> logging.handlers.QueueListener:
+    lq = Queue(maxsize=10000)
+    queue_handler = logging.handlers.QueueHandler(lq)
+    queue_handler.setLevel(logging.DEBUG)
+
+    std_formatter = logging.Formatter(
+        "[%(levelname)s|%(asctime)s|%(module)s|L%(lineno)d] %(message)s"
+    )
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.setFormatter(std_formatter)
+
+    Path("logs").mkdir(exist_ok=True)
+    file_handler = logging.handlers.RotatingFileHandler(
+        "logs/bot.log", maxBytes=10000000, backupCount=5
+    )
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(std_formatter)
+
+    listener = logging.handlers.QueueListener(
+        lq, stdout_handler, file_handler, respect_handler_level=False
+    )
+    listener.start()
+
+    logging.root.addHandler(queue_handler)
+    logging.root.setLevel(logging.DEBUG)
+
+    return listener
+
+
+def get_logger() -> logging.Logger:
+    return logging.getLogger("bot_logger")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import sys
 from aiogram import Bot, Dispatcher, Router
 from aiogram.types import Message
 from aiogram.methods.send_message import SendMessage
@@ -8,21 +7,35 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from config import BOT_API_KEY
+from logger import setup_logging, get_logger
 
 
 router = Router(name=__name__)
 
 
 @router.message()
-async def hello(message: Message) -> SendMessage:
+async def hello(message: Message, logger: logging.Logger) -> SendMessage:
     current_user = message.from_user
     if current_user is None:
+        logger.error("there is not current user")
         return message.answer("Oops, something went wrong")
+    logger.info("Just a test log")
     return message.answer(f"Hello, {current_user.full_name}")
+
+
+async def startup_event(dispatcher: Dispatcher) -> None:
+    dispatcher["log_listener"] = setup_logging()
+    dispatcher["logger"] = get_logger()
+
+
+async def shutdown_event(dispatcher: Dispatcher) -> None:
+    dispatcher["log_listener"].stop()
 
 
 async def main() -> None:
     dp = Dispatcher()
+    dp.startup.register(startup_event)
+    dp.shutdown.register(shutdown_event)
     dp.include_routers(router)
     bot = Bot(
         token=BOT_API_KEY,
@@ -32,5 +45,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
