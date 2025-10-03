d7e0c42219dcb13fa5a4e730331df3b023f3d0f9
Steven Rudenko <srudenko@ncl.com>
2025-10-03 20:06:53 +0300

    create basic handler and start up the bot

---
+++ src/config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_API_KEY: str = os.getenv("BOT_API_KEY", default="")
+
+assert BOT_API_KEY != "", "Bot api key should be added to the env variables"
--- src/main.py
+++ src/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from fitness-bot!")
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher, Router
+from aiogram.types import Message
+from aiogram.methods.send_message import SendMessage
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import BOT_API_KEY
+
+
+router = Router(name=__name__)
+
+
+@router.message()
+async def hello(message: Message) -> SendMessage:
+    current_user = message.from_user
+    if current_user is None:
+        return message.answer("Oops, something went wrong")
+    return message.answer(f"Hello, {current_user.full_name}")
+
+
+async def main() -> None:
+    dp = Dispatcher()
+    dp.include_routers(router)
+    bot = Bot(
+        token=BOT_API_KEY,
+        default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN_V2),
+    )
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
