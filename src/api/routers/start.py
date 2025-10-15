ff12b5942196bddb668730197c87c2836056ac99
Steven Rudenko <steven.gucum@gmail.com>
2025-10-15 18:30:52 +0300

    add manual timezone selection

--- src/api/routers/start.py
+++ src/api/routers/start.py
@@ -7,7 +7,13 @@ from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.methods import SendMessage
-from aiogram.types import KeyboardButton, Location, Message, ReplyKeyboardMarkup
+from aiogram.types import (
+    KeyboardButton,
+    Location,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
 
 from config import config
 from db.session import SessionMaker
@@ -54,9 +60,10 @@ async def get_height(
     await state.update_data(height=height)
     await state.set_state(StartStates.EnterTimezone)
     return message.answer(
-        "Now we need to learn your timezone\. "  # pyright: ignore[reportInvalidStringEscapeSequence]
+        "Now we need to learn your timezone\. "
         "Please share your location "
-        "\(we will only save your timezone based on the location\)",  # pyright: ignore[reportInvalidStringEscapeSequence]
+        "\(we will only save your timezone based on the location\) "
+        "or text a GMT hour offset, for example `\+9` for GMT\+9 or `\-0` for UTC",
         reply_markup=ReplyKeyboardMarkup(
             keyboard=[[KeyboardButton(text="Share my Location", request_location=True)]]
         ),
@@ -70,6 +77,30 @@ def incorrect_height(message: Message) -> SendMessage:
     )
 
 
+@router.message(StartStates.EnterTimezone, F.text.regexp(r"^[\+-]\d{1,2}$").as_("gmt"))
+async def timezone_from_gmt(
+    message: Message,
+    state: FSMContext,
+    session_maker: SessionMaker,
+    logger: Logger,
+    gmt: Match[str],
+) -> SendMessage:
+    if abs(int(gmt.string)) > 12:
+        return message.answer("The specified GMT should be between \-12 and \+12")
+    timezone: str = "Etc/GMT" + gmt.string
+    data: dict[str, Any] = await state.get_data()
+    await state.clear()
+    user = await create_user(
+        data["telegram_user"], session_maker, logger, data["height"], timezone
+    )
+    logger.info(f"created new user for telegram id {user.telegram_id}")
+    return message.answer(
+        f"Your timezone is set to be {user.timezone.replace('+', '\+')}\n"
+        f"Welcome {user.full_name}\!",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
 @router.message(StartStates.EnterTimezone, F.location.as_("location"))
 async def timezone_from_location(
     message: Message,
@@ -78,28 +109,32 @@ async def timezone_from_location(
     logger: Logger,
     location: Location,
 ) -> SendMessage:
-    data: dict[str, Any] = await state.get_data()
-    await state.clear()
-    logger.debug(f"{location.latitude = }; {location.longitude = }")
     timezone: str
     try:
         timezone = get_timezone_from_location(location)
     except TimezoneApiError as e:
         logger.error(f"timezone api error: {e}")
-        timezone = "UTC"
+        return message.answer(
+            "Could not get your timezone, please enter it manually",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+    data: dict[str, Any] = await state.get_data()
+    await state.clear()
+    logger.debug(f"{location.latitude = }; {location.longitude = }")
     user = await create_user(
         data["telegram_user"], session_maker, logger, data["height"], timezone
     )
     logger.info(f"created new user for telegram id {user.telegram_id}")
     return message.answer(
-        f"Your timezone is set to be {user.timezone}\nWelcome {user.full_name}"
+        f"Your timezone is set to be {user.timezone}\nWelcome {user.full_name}\!",
+        reply_markup=ReplyKeyboardRemove(),
     )
 
 
 @router.message(StartStates.EnterTimezone)
 def no_location(message: Message) -> SendMessage:
     return message.answer(
-        "Please send a location",
+        "Please send a location or specify the correct GMT, for example \+9",
         reply_markup=ReplyKeyboardMarkup(
             keyboard=[[KeyboardButton(text="Share my Location", request_location=True)]]
         ),
