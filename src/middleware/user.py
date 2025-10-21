f10d8f2d59d65a27b70e8d7d2dbad9b604154595
Steven Rudenko <steven.gucum@gmail.com>
2025-10-21 14:56:21 +0300

    change the error messages to use the message_loader

--- src/middleware/user.py
+++ src/middleware/user.py
@@ -2,28 +2,31 @@ from logging import Logger
 
 from aiogram.methods.send_message import SendMessage
 from aiogram.types import ErrorEvent
+from message_loader.main import MessageLoader
 
 from ..models.user import UserNotRegisteredError
 
 
-async def no_user_error(event: ErrorEvent, logger: Logger) -> SendMessage | None:
+async def no_user_error(
+    event: ErrorEvent, logger: Logger, message_loader: MessageLoader
+) -> SendMessage | None:
     logger.error("no user found")
     return (
-        event.update.message.answer("ERROR: no user found")
+        event.update.message.answer(message_loader.render_msg("error_no_user"))
         if event.update.message
         else None
     )
 
 
 async def user_not_registered_error(
-    event: ErrorEvent, logger: Logger
+    event: ErrorEvent, logger: Logger, message_loader: MessageLoader
 ) -> SendMessage | None:
     if not isinstance(event.exception, UserNotRegisteredError):
         return
     logger.warning(f"user {event.exception.telegram_id} is not registered")
     return (
         event.update.message.answer(
-            "We could not find your user\. Please register using /start command"  # pyright: ignore[reportInvalidStringEscapeSequence]
+            message_loader.render_msg("warn_user_not_registered")
         )
         if event.update.message
         else None
